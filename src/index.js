62c10993569dd7b5779a8230e3552dcf4307ca9d
chskela <chskela@mail.ru>
2019-06-25 21:32:09 +0300

    https://github.com/chskela/Countdown-Timer/issues/4

--- src/index.js
+++ src/index.js
@@ -12,15 +12,10 @@ const warning = document.querySelector('#warning');
 
 const dateNow = new Date();
 
-const month = (dateNow.getMonth()+1 < 10) ? ('0' + (dateNow.getMonth()+1)) : ('' + (dateNow.getMonth()+1));
+const month = (dateNow.getMonth() + 1 < 10) ? ('0' + (dateNow.getMonth() + 1)) : ('' + (dateNow.getMonth() + 1));
 const day = (dateNow.getDate() < 10) ? ('0' + dateNow.getDate()) : ('' + dateNow.getDate());
-// const hours = (dateNow.getHours() < 10) ? ('0' + dateNow.getHours()) : ('' + dateNow.getHours());
-// const minutes = (dateNow.getMinutes() < 10) ? ('0' + dateNow.getMinutes()) : ('' + dateNow.getMinutes());
-// const seconds = (dateNow.getSeconds() < 10) ? ('0' + dateNow.getSeconds()) : ('' + dateNow.getSeconds());
 
-
-dateControl.value = dateNow.getFullYear()+'-'+ month +'-'+day;
-//timeControl.value = '00:00:00';//hours + ':' + minutes + ':' + seconds;
+dateControl.value = dateNow.getFullYear() + '-' + month + '-' + day;
 
 const calculationTime = intervalInSeconds => {
 
@@ -41,19 +36,29 @@ const calculationTime = intervalInSeconds => {
 };
 
 start.addEventListener('click', () => {
+
     if (nameControl.value === '') {
         warning.textContent = 'Введите название события!';
     } else {
-        clock.hidden = false;
+
         timerName.textContent = nameControl.value;
 
-        intervalInSeconds = Math.floor(Math.abs((+new Date(dateControl.value + ' ' + timeControl.value) - new Date()) / 1000));
-        console.log(intervalInSeconds);
-        let timerId = setInterval(() => {
-            if(intervalInSeconds <= 0) clearInterval(timerId);
-            calculationTime(intervalInSeconds);
-            intervalInSeconds--;
-        }, 1000)
+        const setTime = new Date(dateControl.value + ' ' + timeControl.value);
+        const nowTime = new Date();
+
+        if (setTime <= nowTime)  {
+            warning.textContent = 'Введите корректную дату и время!';
+        } else {
+            clock.hidden = false;
+            intervalInSeconds = Math.floor(Math.abs((setTime - nowTime) / 1000));
+
+            let timerId = setInterval(() => {
+                if(intervalInSeconds <= 0) clearInterval(timerId);
+                calculationTime(intervalInSeconds);
+                intervalInSeconds--;
+            }, 1000)
+        }
+
     }
 
 });
